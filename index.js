68880aef1de8756b0f34003b06a7135fc74273c3
AlexBand-Pro <tillromutni@gmail.com>
2025-05-14 15:49:44 -0600

    complete project

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const tl = gsap.timeline()
 
-tl.from("h1", {duration: 0.5, x: -300, opacity: 0})
-tl.from(".tagline", {duration: 0.5, x: 300, opacity: 0}, "=-0.5")
-tl.from(".in-text-hero-img-container", {duration: 0.5, y: 100, opacity: 0})
-tl.from(".hero-link", {duration: 0.5, y: 40, opacity: 0})
+tl.to("h1", {duration: 0.5, x: 0, opacity: 1})
+tl.to(".tagline", {duration: 0.5, x: 0, opacity: 1}, "=-0.5")
+tl.to(".in-text-hero-img-container", {duration: 0.5, y: 0, opacity: 1})
+tl.to(".hero-img-container", {duration: 0.5, y: 0, opacity: 1}, "=-0.5")
+tl.to(".hero-link", {duration: 0.5, y: 0, opacity: 1})
 
 // Set initial styles
-gsap.set(".about-img", { y: 100, opacity: 0 })
-gsap.set(".about-text", { y: 100, opacity: 0 })
-gsap.set(".poem", { y: 100, opacity: 0 })
+// gsap.set(".about-img", { y: 100, opacity: 0 })
+// gsap.set(".about-text", { y: 100, opacity: 0 })
+// gsap.set(".poem", { y: 100, opacity: 0 })
 
 // Create separate timelines
 gsap.to(".about-img", {
@@ -52,7 +53,7 @@ const split = new SplitText(".poem-text div", {
 gsap.from(split.chars, {
   scrollTrigger: {
     trigger: ".poem-text",
-    start: "top 80%",
+    start: "top 100%",
   },
   opacity: 0,
   y: 20,
@@ -67,4 +68,13 @@ gsap.set("form", { y: 100, opacity: 0 })
 gsap.to(".contact-heading", {duration: 0.5, opacity: 1, y: 0, scrollTrigger: {trigger: ".contact-heading"}})
 gsap.to("form", {duration: 0.5, opacity: 1, y: 0, scrollTrigger: {trigger: "form"}})
 
-gsap.from(".social-media-link", {duration: 0.5, scale: 0, scrollTrigger: {trigger: ".social-media-link"}, stagger: 0.3, ease: "back.out"})
+gsap.from(".social-media-link", {duration: 0.5, scale: 0, scrollTrigger: {trigger: ".social-media-link"}, stagger: 0.2, ease: "back.out"})
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault() //remove with formspree
+  setTimeout(() => {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+  }, 3000);
+});
